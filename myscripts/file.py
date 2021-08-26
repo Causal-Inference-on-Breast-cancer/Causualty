fcb84b62cc01abb370d59dcb085c55c6609c0a3f
NatnaelSisay <natyjava8@gmail.com>
2021-08-26 15:59:42 +0300

    acreated essential folders and files

---
+++ myscripts/file.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from my_logger import logger
+
+def read_csv(file_name):
+  try:
+    logger.info(f"{file_name} : Reading")
+    null_values = ["n/a", "na", "undefined"]
+    df = pd.read_csv(f'../data/{file_name}', na_values=null_values)
+    logger.info("Finished Reading File!")
+    return df
+  except:
+    logger.error(f"File Not Found : {file_name}")
